64548824b8285785faec5facd8802f9fcd332d50
Quan666 <i@Rori.EMail>
2020-09-28 15:20:52 +0800

    add more api

--- main.js
+++ main.js
@@ -1,37 +1,67 @@
 sleep(2000)
 toast('开始');
+var APIKEY = "Z3qi2q";
+var apiLink = [
+    "https://v1.hitokoto.cn",
+    "https://nmsl.shadiao.app/api.php?from=" + APIKEY,
+    "https://nmsl.shadiao.app/api.php?level=min&from=" + APIKEY,
+    "https://chp.shadiao.app/api.php?from=" + APIKEY,
+    "https://pyq.shadiao.app/api.php?from=" + APIKEY,
+    "https://du.shadiao.app/api.php?from=" + APIKEY
+];
+// 火力全开：https://nmsl.shadiao.app/api.php?from=xxx
+// 口吐莲花：https://nmsl.shadiao.app/api.php?level=min&from=xxx
+// 彩虹屁生成器：https://chp.shadiao.app/api.php?from=xxx
+// 朋友圈文案生成器：https://pyq.shadiao.app/api.php?from=xxx
+// 毒鸡汤生成器：https://du.shadiao.app/api.php?from=xxx
 
-var i = 0;
-var Count = 1;
-Count = rawInput("发送条数", 1);
-var sleepTime=5000
-sleepTime = rawInput("休息时间", 5000);
-var flag = true;
-var fuck=false;
-var clear = confirm("要开启骂人模式吗?");
-if(clear){
-    fuck=true;
+
+var select=false;
+var select_index=0;
+var d = dialogs.build({
+    title: "请选择模式",
+    positive: "确定",
+    negative: "取消",
+    items: ["一言", "火力全开", "口吐莲花", "彩虹屁生成器", "朋友圈文案生成器", "毒鸡汤生成器"],
+    itemsSelectMode: "select"
+}).on("item_select", (index, item, dialog) => {
+    toast("您选择的是: " + item);
+    select_index=index;
+    select=true;
+}).show();
+while(!select){
+    sleep(100);
 }
 
-var fuckAPI='https://zuanbot.com/api.php?level=min&lang=zh_cn&from=Z3qi2q';
-var dayOne='https://v1.hitokoto.cn';
-while (flag) {
+sendMsg();
 
-    http.get(fuck?fuckAPI:dayOne, {}, function (res, err) {
-        if (err) {
-            console.error(err);
-            return;
-        }
+function sendMsg() {
+    var i = 0;
+    var Count = 1;
+    Count = rawInput("发送条数", 1);
+    var sleepTime = 5000
+    sleepTime = rawInput("休息时间", 5000);
+    var flag = true;
+    while (flag) {
 
-        var json = fuck?res.body.string():res.body.json().hitokoto;
-        if (i >= Count) {
-            flag = false;
-            return;
-        }
-        i++;
-        setText(json);
-        text("发送").click();
-        
-    });
-    sleep(sleepTime);
+        http.get(select_index!=0 ? apiLink[select_index] : apiLink[0], {}, function (res, err) {
+            if (err) {
+                console.error(err);
+                return;
+            }
+
+            var json = select_index!=0  ? res.body.string() : res.body.json().hitokoto;
+            if (i >= Count) {
+                flag = false;
+                return;
+            }
+            i++;
+            setText(json);
+            text("发送").click();
+
+        });
+        sleep(sleepTime);
+    }
 }
+
+
